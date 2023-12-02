f47f8377c57b9eed68cb54e0de3d704890719925
Kenedy Jardim <kenedydacosta@gmail.com>
2023-12-01 21:27:59 -0300

    estilizando a sidebar

--- src/conteires/Sidebar/index.tsx
+++ src/conteires/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Taironius.png" />
-    <Titulo fontSize={20}>Kenedy Jardim</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Kenedy Jardim</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Taironius
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
