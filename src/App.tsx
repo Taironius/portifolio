c212d09fa9e7d05b2c39710e903437b555f4a602
Kenedy Jardim <kenedydacosta@gmail.com>
2023-12-01 22:42:25 -0300

    finalização do projeto :-)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './conteires/Projetos'
 import Sidebar from './conteires/Sidebar'
 import Sobre from './conteires/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -11,6 +11,7 @@ const EstiloGlobal = createGlobalStyle`
   body {
     padding-top: 80px;
     padding-bottom: 80px;
+    background-color: ${(props) => props.theme.corDeFundo};
 
     @media (max-width: 768px) {
 
@@ -32,9 +33,5 @@ export const Container = styled.div`
   @media (max-width: 768px) {
     max-width: 80%;
     display: block;
-
-    img {
-      width: 100%;
-    }
   }
 `
